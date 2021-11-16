3d2a5083ee04a63150ad3c8cd81fdfa19e66798d
brentper <60619374+brentper@users.noreply.github.com>
2021-11-16 10:46:16 -0500

    Add contact us page (#5)

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using PizzaCore.Models;
 
 namespace PizzaCore.Data {
   public class ApplicationDbContext : IdentityDbContext {
---
+++ Services/GoogleServicesOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaCore.Services {
+  public class GoogleServicesOptions {
+    public string ReCaptchaApiKey { get; set; }
+    public string MapsApiKey { get; set; }
+  }
+}
