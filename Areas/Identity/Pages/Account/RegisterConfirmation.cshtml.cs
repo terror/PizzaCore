7fb4f0ec32f3a2755d30958e80161f65311771e2
terror <31192478+terror@users.noreply.github.com>
2021-11-17 19:34:28 -0500

    Format code and remove unnecessary usings (#10)

--- Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
+++ Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
@@ -1,11 +1,11 @@
 using Microsoft.AspNetCore.Authorization;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace PizzaCore.Areas.Identity.Pages.Account {
   [AllowAnonymous]
