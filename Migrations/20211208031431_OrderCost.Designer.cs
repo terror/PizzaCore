4c35f36b7cf8d4dd6ff829e8fc0bc25d7c2c089a
Liam Scalzulli <liam@scalzulli.com>
2021-12-15 22:27:06 -0500

    format and fix userdata migration

--- Migrations/20211208031431_OrderCost.Designer.cs
+++ Migrations/20211208031431_OrderCost.Designer.cs
@@ -7,221 +7,211 @@ using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PizzaCore.Data;
 
-namespace PizzaCore.Migrations
-{
-    [DbContext(typeof(PizzaCoreContext))]
-    [Migration("20211208031431_Order-Cost")]
-    partial class OrderCost
-    {
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
-        {
+namespace PizzaCore.Migrations {
+  [DbContext(typeof(PizzaCoreContext))]
+  [Migration("20211208031431_Order-Cost")]
+  partial class OrderCost {
+    protected override void BuildTargetModel(ModelBuilder modelBuilder) {
 #pragma warning disable 612, 618
-            modelBuilder
-                .HasAnnotation("Relational:MaxIdentifierLength", 128)
-                .HasAnnotation("ProductVersion", "5.0.12")
-                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+      modelBuilder
+          .HasAnnotation("Relational:MaxIdentifierLength", 128)
+          .HasAnnotation("ProductVersion", "5.0.12")
+          .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-            modelBuilder.Entity("PizzaCore.Data.Entities.Product", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int")
-                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+      modelBuilder.Entity("PizzaCore.Data.Entities.Product", b => {
+        b.Property<int>("Id")
+            .ValueGeneratedOnAdd()
+            .HasColumnType("int")
+            .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-                    b.Property<string>("Category")
-                        .HasColumnType("nvarchar(max)");
+        b.Property<string>("Category")
+            .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("Description")
-                        .HasColumnType("nvarchar(max)");
+        b.Property<string>("Description")
+            .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("ImageId")
-                        .HasColumnType("nvarchar(max)");
+        b.Property<string>("ImageId")
+            .HasColumnType("nvarchar(max)");
 
-                    b.Property<bool>("IsFeatured")
-                        .HasColumnType("bit");
+        b.Property<bool>("IsFeatured")
+            .HasColumnType("bit");
 
-                    b.Property<string>("Name")
-                        .HasColumnType("nvarchar(max)");
+        b.Property<string>("Name")
+            .HasColumnType("nvarchar(max)");
 
-                    b.HasKey("Id");
+        b.HasKey("Id");
 
-                    b.ToTable("Products");
-                });
+        b.ToTable("Products");
+      });
 
-            modelBuilder.Entity("PizzaCore.Data.Entities.ProductSize", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int")
-                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+      modelBuilder.Entity("PizzaCore.Data.Entities.ProductSize", b => {
+        b.Property<int>("Id")
+            .ValueGeneratedOnAdd()
+            .HasColumnType("int")
+            .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-                    b.Property<double>("Price")
-                        .HasColumnType("float");
+        b.Property<double>("Price")
+            .HasColumnType("float");
 
-                    b.Property<int>("ProductId")
-                        .HasColumnType("int");
+        b.Property<int>("ProductId")
+            .HasColumnType("int");
 
-                    b.Property<string>("Size")
-                        .HasColumnType("nvarchar(max)");
+        b.Property<string>("Size")
+            .HasColumnType("nvarchar(max)");
 
-                    b.HasKey("Id");
+        b.HasKey("Id");
 
-                    b.HasIndex("ProductId");
+        b.HasIndex("ProductId");
 
-                    b.ToTable("ProductSizes");
-                });
+        b.ToTable("ProductSizes");
+      });
 
-            modelBuilder.Entity("PizzaCore.Models.CareersModel", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int")
-                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+      modelBuilder.Entity("PizzaCore.Models.CareersModel", b => {
+        b.Property<int>("Id")
+            .ValueGeneratedOnAdd()
+            .HasColumnType("int")
+            .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-                    b.Property<byte[]>("CVBinary")
-                        .HasColumnType("varbinary(max)");
+        b.Property<byte[]>("CVBinary")
+            .HasColumnType("varbinary(max)");
 
-                    b.Property<DateTime>("Date")
-                        .HasColumnType("datetime2");
+        b.Property<DateTime>("Date")
+            .HasColumnType("datetime2");
 
-                    b.Property<string>("Email")
-                        .IsRequired()
-                        .HasMaxLength(30)
-                        .HasColumnType("nvarchar(30)");
+        b.Property<string>("Email")
+            .IsRequired()
+            .HasMaxLength(30)
+            .HasColumnType("nvarchar(30)");
 
-                    b.Property<string>("FirstName")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+        b.Property<string>("FirstName")
+            .IsRequired()
+            .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("LastName")
-                        .IsRequired()
-                        .HasMaxLength(50)
-                        .HasColumnType("nvarchar(50)");
+        b.Property<string>("LastName")
+            .IsRequired()
+            .HasMaxLength(50)
+            .HasColumnType("nvarchar(50)");
 
-                    b.Property<string>("Position")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+        b.Property<string>("Position")
+            .IsRequired()
+            .HasColumnType("nvarchar(max)");
 
-                    b.HasKey("Id");
+        b.HasKey("Id");
 
-                    b.ToTable("CareersModels");
-                });
+        b.ToTable("CareersModels");
+      });
 
-            modelBuilder.Entity("PizzaCore.Models.ContactModel", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int")
-                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+      modelBuilder.Entity("PizzaCore.Models.ContactModel", b => {
+        b.Property<int>("Id")
+            .ValueGeneratedOnAdd()
+            .HasColumnType("int")
+            .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-                    b.Property<DateTime>("Date")
-                        .HasColumnType("datetime2");
+        b.Property<DateTime>("Date")
+            .HasColumnType("datetime2");
 
-                    b.Property<string>("Email")
-                        .IsRequired()
-                        .HasMaxLength(30)
-                        .HasColumnType("nvarchar(30)");
+        b.Property<string>("Email")
+            .IsRequired()
+            .HasMaxLength(30)
+            .HasColumnType("nvarchar(30)");
 
-                    b.Property<string>("FirstName")
-                        .IsRequired()
-                        .HasMaxLength(50)
-                        .HasColumnType("nvarchar(50)");
+        b.Property<string>("FirstName")
+            .IsRequired()
+            .HasMaxLength(50)
+            .HasColumnType("nvarchar(50)");
 
-                    b.Property<string>("LastName")
-                        .IsRequired()
-                        .HasMaxLength(50)
-                        .HasColumnType("nvarchar(50)");
+        b.Property<string>("LastName")
+            .IsRequired()
+            .HasMaxLength(50)
+            .HasColumnType("nvarchar(50)");
 
-                    b.Property<string>("Message")
-                        .IsRequired()
-                        .HasMaxLength(500)
-                        .HasColumnType("nvarchar(500)");
+        b.Property<string>("Message")
+            .IsRequired()
+            .HasMaxLength(500)
+            .HasColumnType("nvarchar(500)");
 
-                    b.Property<string>("Topic")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+        b.Property<string>("Topic")
+            .IsRequired()
+            .HasColumnType("nvarchar(max)");
 
-                    b.HasKey("Id");
+        b.HasKey("Id");
 
-                    b.ToTable("ContactModels");
-                });
+        b.ToTable("ContactModels");
+      });
 
-            modelBuilder.Entity("PizzaCore.Models.OrderModel", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int")
-                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+      modelBuilder.Entity("PizzaCore.Models.OrderModel", b => {
+        b.Property<int>("Id")
+            .ValueGeneratedOnAdd()
+            .HasColumnType("int")
+            .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-                    b.Property<string>("Address")
-                        .HasColumnType("nvarchar(max)");
+        b.Property<string>("Address")
+            .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("City")
-                        .HasMaxLength(50)
-                        .HasColumnType("nvarchar(50)");
+        b.Property<string>("City")
+            .HasMaxLength(50)
+            .HasColumnType("nvarchar(50)");
 
-                    b.Property<DateTime>("Date")
-                        .HasColumnType("datetime2");
+        b.Property<DateTime>("Date")
+            .HasColumnType("datetime2");
 
-                    b.Property<string>("Email")
-                        .IsRequired()
-                        .HasMaxLength(30)
-                        .HasColumnType("nvarchar(30)");
+        b.Property<string>("Email")
+            .IsRequired()
+            .HasMaxLength(30)
+            .HasColumnType("nvarchar(30)");
 
-                    b.Property<string>("FirstName")
-                        .IsRequired()
-                        .HasMaxLength(50)
-                        .HasColumnType("nvarchar(50)");
+        b.Property<string>("FirstName")
+            .IsRequired()
+            .HasMaxLength(50)
+            .HasColumnType("nvarchar(50)");
 
-                    b.Property<string>("LastName")
-                        .IsRequired()
-                        .HasMaxLength(50)
-                        .HasColumnType("nvarchar(50)");
+        b.Property<string>("LastName")
+            .IsRequired()
+            .HasMaxLength(50)
+            .HasColumnType("nvarchar(50)");
 
-                    b.Property<string>("Method")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+        b.Property<string>("Method")
+            .IsRequired()
+            .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("Payment")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+        b.Property<string>("Payment")
+            .IsRequired()
+            .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("Phone")
-                        .HasColumnType("nvarchar(max)");
+        b.Property<string>("Phone")
+            .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("PostalCode")
-                        .HasColumnType("nvarchar(max)");
+        b.Property<string>("PostalCode")
+            .HasColumnType("nvarchar(max)");
 
-                    b.Property<double>("ShippingCost")
-                        .HasColumnType("float");
+        b.Property<double>("ShippingCost")
+            .HasColumnType("float");
 
-                    b.Property<double>("SubTotal")
-                        .HasColumnType("float");
+        b.Property<double>("SubTotal")
+            .HasColumnType("float");
 
-                    b.Property<double>("Taxes")
-                        .HasColumnType("float");
+        b.Property<double>("Taxes")
+            .HasColumnType("float");
 
-                    b.HasKey("Id");
+        b.HasKey("Id");
 
-                    b.ToTable("OrderModels");
-                });
+        b.ToTable("OrderModels");
+      });
 
-            modelBuilder.Entity("PizzaCore.Data.Entities.ProductSize", b =>
-                {
-                    b.HasOne("PizzaCore.Data.Entities.Product", "Product")
-                        .WithMany("ProductSizes")
-                        .HasForeignKey("ProductId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
+      modelBuilder.Entity("PizzaCore.Data.Entities.ProductSize", b => {
+        b.HasOne("PizzaCore.Data.Entities.Product", "Product")
+            .WithMany("ProductSizes")
+            .HasForeignKey("ProductId")
+            .OnDelete(DeleteBehavior.Cascade)
+            .IsRequired();
 
-                    b.Navigation("Product");
-                });
+        b.Navigation("Product");
+      });
 
-            modelBuilder.Entity("PizzaCore.Data.Entities.Product", b =>
-                {
-                    b.Navigation("ProductSizes");
-                });
+      modelBuilder.Entity("PizzaCore.Data.Entities.Product", b => {
+        b.Navigation("ProductSizes");
+      });
 #pragma warning restore 612, 618
-        }
     }
+  }
 }
